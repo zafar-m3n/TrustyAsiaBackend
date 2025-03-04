d76585aefb6d3ccae803df0fdfcef2a2b554d190
zafar-m3n <thezafar.m3n@gmail.com>
2025-03-04 17:06:22 +0530

    Login Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,7 +48,48 @@ const registerController = async (req, res) => {
   }
 };
 
-const loginController = () => {};
+const loginController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!user) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.NODE_TRUSTYASIA_JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).send({
+      success: true,
+      message: "Login Successfully",
+      token: token,
+    });
+
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: `Internal Server Error ${error.message}`,
+    });
+  }
+};
 
 module.exports = {
   loginController,
