105f9dd653475d5bc5d330eac27ae2ac3d7bfc4c
zafar-m3n <thezafar.m3n@gmail.com>
2025-03-11 18:57:04 +0530

    Headers

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use(
     origin: ["https://trustyasia.com", "http://localhost:5173"],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
